f9a613ec9aaa6fa2eea251e74beb2f0935cccf19
Cristhian <cristhianandres2001@gmail.com>
2023-04-25 08:35:43 -0500

    Primer commit, Contruccion de proyecto modular

---
+++ src/main/java/com/example/ecommercebitech/EcommerceBitechApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBitechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBitechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecommercebitech/EcommerceBitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercebitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBitechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
